8ced57d0b457a7ab00f56a2ef616f71c6dde637a
John Vincent <corcegajovic@gmail.com>
2021-05-07 10:53:10 +0800

    JS fix for feedback

--- app/assets/feedback.js
+++ app/assets/feedback.js
@@ -24,52 +24,21 @@ function checkSelect(select) {
     }
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-  var form = document.getElementById("frsh_feedback_form");
-  var myForm = document.getElementById("frsh_feedback_form");
-  var button = document.getElementById("submit-button");
-  var status = document.getElementById("main_form");
+$('#form').submit(function (event) {
+    event.preventDefault()
 
-  function success() {
-    status.setAttribute("disabled", "disabled");
-    button.textContent = 'Submitted successfully';
+    var button = document.getElementById("submit-button");
+    var status = document.getElementById("main_form");
+
     button.disabled = true;
-  }
-
-  function error() {
-    button.textContent = 'Oops! There was a problem.';
-  }
-
-  // handle the form submission event
-  if (form != null) {
-    form.addEventListener("submit", function (ev) {
-      ev.preventDefault();
-      var data = new FormData();
-      data.append("entry.1811482587", document.querySelector("#frsh_reporter").value);
-      data.append("entry.1172745133", document.querySelector("#frsh_device").value);
-      data.append("entry.698861321", document.querySelector("#feedback_selection").value);
-      data.append("entry.1702090977", document.querySelector("#information").value);
-      ajax(form.method, data, success, error);
-    });
-  }
-});
-
-// helper function for sending an AJAX request
-function ajax(method, data, success, error) {
+    var extraData = {}
     $('#form').ajaxSubmit({
         data: extraData,
         dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
         error: function () {
-            success();
+          status.setAttribute("disabled", "disabled");
+          button.textContent = 'Submitted successfully';
+          button.disabled = true;
         }
     })
-}
-
-function getInputData(){
-    let dataToPost = new FormData(); //formdata API
-
-    //fill name attributes to corresponding values
-    dataToPost.append("entry.294341084", document.querySelector("#inp1").value);
-
-    return dataToPost;
-}
+})
