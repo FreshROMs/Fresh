790fcb7f3cd131b5305c4a0c322e10ebc383c739
John Vincent <corcegajovic@gmail.com>
2021-05-07 11:07:19 +0800

    OMC request fix

--- app/assets/omc.js
+++ app/assets/omc.js
@@ -1,36 +1,19 @@
 
-window.addEventListener("DOMContentLoaded", function () {
-  var form = document.getElementById("frsh_omc_form");
-  var myForm = document.getElementById("frsh_omc_form");
-  var button = document.getElementById("submit-button");
-  var status = document.getElementById("main_form");
+$('#form').submit(function (event) {
+    event.preventDefault()
 
-  function success() {
-    status.setAttribute("disabled", "disabled");
-    button.textContent = 'Submitted successfully';
+    var button = document.getElementById("submit-button");
+    var status = document.getElementById("main_form");
+
     button.disabled = true;
-  }
-
-  function error() {
-    button.textContent = 'Oops! There was a problem.';
-  }
-
-  // handle the form submission event
-  if (form != null) {
-    form.addEventListener("submit", function (ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, data, success, error);
-    });
-  }
-});
-
-// helper function for sending an AJAX request
-function ajax(method, data, success, error) {
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "multipart/form-data" },
-    body: new URLSearchParams(data).toString()
-  }).then(() => success()).catch((error) => {
-    console.log(error); error()})
-}
+    var extraData = {}
+    $('#form').ajaxSubmit({
+        data: extraData,
+        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+        error: function () {
+          status.setAttribute("disabled", "disabled");
+          button.textContent = 'Submitted successfully';
+          button.disabled = true;
+        }
+    })
+})
