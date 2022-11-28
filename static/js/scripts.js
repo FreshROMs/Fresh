12adafa7b2b2187018793eac3f14baa1be186e21
John Vincent <git@tenseventyseven.cf>
2022-11-28 11:28:03 +0800

    website: Release for Fresh 4
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,92 @@
 $(function () {
-    var now = new Date();
-    var day = now.getDate();
-    var month = now.getMonth();
-    var year = now.getFullYear() + 1;
 
-    var nextyear = month + '/' + day + '/' + year + ' 07:07:07';
+    // init feather icons
+    feather.replace();
 
-    $('#timer').countdown({
-        date: "12/9/2022 7:00:00", //TODO Date format: 07/27/2017 17:00:00
-        offset: +8,
-        day: 'Day',
-        days: 'Days'
-    }, function () {
+    // init tooltip & popovers
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+    //page scroll
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 50
+        }, 1000);
+        event.preventDefault();
     });
+
+    //toggle scroll menu
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        // adjust scroll to top
+        if (scroll >= 600) {
+            $('.scroll-top').addClass('active');
+        } else {
+            $('.scroll-top').removeClass('active');
+        }
+        return false;
+    });
+    
+    //open / close video section
+    $('.show-video').click(function(){
+        $('.video-wrapper').addClass('show');
+    });
+    
+    $('.video-close').click(function(){
+        $('.video-wrapper').removeClass('show');
+    });
+
+    // slick slider
+    $('.slick-users').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: false
+    });
+
+    // scroll top top
+    $('.scroll-top').click(function () {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    /**Theme switcher - DEMO PURPOSE ONLY */
+    $('.switcher-trigger').click(function () {
+        $('.switcher-wrap').toggleClass('active');
+    });
+    $('.color-switcher ul li').click(function () {
+        var color = $(this).attr('data-color');
+        $('#theme-color').attr("href", "css/" + color + ".css");
+        $('.color-switcher ul li').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
